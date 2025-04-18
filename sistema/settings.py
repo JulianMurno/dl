4932b373a8938407bc32f6485c856c8ab8d4191d
JulianMurno <julianmurno@iresm.edu.ar>
2025-04-18 17:55:32 -0300

    Fix settings.py syntax

--- sistema/settings.py
+++ sistema/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 
 from pathlib import Path
 import os
-
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -75,7 +75,7 @@ WSGI_APPLICATION = 'sistema.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
-import dj_database_url
+
 
 DATABASES = {
     'default': dj_database_url.config(
@@ -83,8 +83,6 @@ DATABASES = {
         conn_max_age=600,
         ssl_require=True
     )
-}
-
 }
 
 
